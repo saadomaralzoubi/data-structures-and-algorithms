0e5a452ce21f36d7fe8c3b5eb0b7ae2a925b6846
saadalzoubi <saad.alzoubi.1994@gmail.com>
2022-03-05 23:21:32 +0200

    add

--- javascript/code-challenges/linked-list/index.js
+++ javascript/code-challenges/linked-list/index.js
@@ -1,5 +1,48 @@
 "use strict";
+const Node = require("./node");
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
-class LinkedList {}
+  insert(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+  }
+  include(value) {
+    if (this.head !== null) {
+      let currentValue = this.head;
+      while (currentValue !== null) {
+        if (currentValue.value === value) {
+          return true;
+        } else {
+          currentValue = currentValue.next;
+        }
+      }
+      return false;
+    } else {
+      return false;
+    }
+  }
+  toString() {
+    let nodeString = "";
+    if (this.head !== null) {
+      let current = this.head;
+      while (current !== null) {
+        nodeString = nodeString + `{ ${current.value} } -> `;
+        current = current.next;
+      }
+      nodeString = nodeString + "NULL";
+      return nodeString;
+    } else {
+      return "empty Linked List";
+    }
+  }
+}
 
 module.exports = LinkedList;
